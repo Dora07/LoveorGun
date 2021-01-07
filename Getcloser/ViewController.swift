6e6d5dd2c822908828eec04c0a7cf92920db7f3c
Dora <domonshieh@gmail.com>
2021-01-07 17:33:41 +0800

    Initial Commit

---
+++ Getcloser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Getcloser
+//
+//  Created by 謝孟芩 on 2021/1/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
